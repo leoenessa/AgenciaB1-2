9db31e0b409131fe817f07e3abfeefd14bf3e4d4
Leonardo <leonardooc@globo.com>
2018-08-01 23:11:47 -0300

    Primeiro envio
    Terminando a secao 1 mobile

---
+++ js/funcionalidades.js
@@ -0,0 +1,32 @@
+$(".top-navigation #menu-principal-mobile-btn").click(function(){
+    var botao =  $(this);
+    var menu = $(".menu-principal-mobile");
+    
+    if(botao.attr("data-aberto") == 0){
+        console.log('0');
+        botao.attr("data-aberto",'1');
+        botao.removeClass('fa-bars');
+        menu.removeClass('menu-invisivel');
+        botao.addClass('fa-times');
+        menu.addClass('menu-visivel');
+    }
+    else if(botao.attr('data-aberto') == 1){
+        console.log('1');
+        botao.attr("data-aberto",'0');
+        botao.removeClass('fa-times');
+        menu.removeClass('menu-visivel');
+        botao.addClass('fa-bars');  
+        menu.addClass('menu-invisivel');
+    }
+  
+});
+
+function b1slideshow(indice_slide,case_target){
+    var i;
+    var slides = $("."+case_target+" .slides .slide");
+    for(i=0; i<slides.length; i++){
+        slides[i].style.display = "none";
+    }
+
+    slides[indice_slide-1].style.display = "block";
+}
